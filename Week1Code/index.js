a20a8a9036c353cecd4234f00c0bcf7108c46ad8
KHN70 <hassankhalid383@gmail.com>
2024-12-25 11:00:38 +0000

    week 1 code

---
+++ Week1Code/index.js
@@ -0,0 +1,21 @@
+const num1 = 5;
+const num2 = 3;
+//add two numbers
+const sum = num1 + num2;
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sum)
+
+const number = sum
+
+//check if number is greater than 0
+if ( number > 0) {
+    console.log("The number is positive");
+}
+
+else if ( number == 0) {
+    console.log("The number is zero");
+}
+
+else{
+    console.log("The number is negative");
+}
